016ec938eb331608a4b3dc60921fa6d7a3b025ed
ธนภัทร เหล่ารอด <palmy@Macintosh.local>
2023-09-29 14:47:40 +0700

    Greeting-components

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,12 @@
+import classes from './Greeting.module.css'
+
+const Greeting = () => {
+  return (
+    <div className={classes.card}>
+      <h3>Welcome!</h3>
+      <p>Palm</p>
+    </div>
+  )
+}
+
+export default Greeting
