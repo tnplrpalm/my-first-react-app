d15dc397db70a6bc38082182586b69f961a76fee
ธนภัทร เหล่ารอด <palmy@Macintosh.local>
2023-10-03 11:22:56 +0700

    demo states

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import classes from './Greeting.module.css'
 
 interface IGreetingProps {
@@ -6,12 +7,21 @@ interface IGreetingProps {
   isLoggedIn: boolean
 }
 
-const Greeting = ({ name, greetingMsg, isLoggedIn }: IGreetingProps) => {
+const Greeting = ({ name, isLoggedIn }: IGreetingProps) => {
+  const [greetingMsg, setGreetingMsg] = useState<string>('Welcome!')
+
+  const handleClick = () => {
+    setGreetingMsg('Hello!')
+  }
+
   return (
-    <div className={classes.card}>
-      <h3>{greetingMsg}</h3>
-      <p>{isLoggedIn ? name : 'Unknown'}</p>
-    </div>
+    <>
+      <div className={classes.card}>
+        <h3>{greetingMsg}</h3>
+        <p>{isLoggedIn ? name : 'Unknown'}</p>
+      </div>
+      <button onClick={handleClick}>Change greeting message</button>
+    </>
   )
 }
 
