78270e41f1052e90117271de618127f97c19fab5
ธนภัทร เหล่ารอด <palmy@Macintosh.local>
2023-10-03 13:36:36 +0700

    toggle2

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -3,27 +3,27 @@ import { PostDTO } from '../types/dto'
 import { useState } from 'react'
 
 interface IPostProps {
-    post:PostDTO
+  post: PostDTO
 }
 
 const Post = ({ post }: IPostProps) => {
-    const [isShow, setShow] = useState<boolean>{false}
-    const handleClick = () => {
-        setShow(!false)
-    }
+  const [isShow, setShow] = useState<boolean>(false)
+  const handleClick = () => {
+    setShow(!false)
+  }
 
-
-
-
-    return (
-        <div className={classes.post}>
-            <p>id:{post.id}</p>
-            <p>postedBy : {post.userId}</p>
-            <p>title: {post.title}</p>
-            <p>body: {post.body}</p>
-            <button onClick={handleClick}>{isShow ? 'Show less' : 'Show more'}</button>
-        </div>
-    )
+  return (
+    <div className={classes.post}>
+      <p>id:{post.id}</p>
+      <p>postedBy : {post.userId}</p>
+      <p>title: {post.title}</p>
+      <p>body: {post.body}</p>
+      {isShow && <p>More information</p>}
+      <button onClick={handleClick}>
+        {isShow ? 'Show less' : 'Show more'}
+      </button>
+    </div>
+  )
 }
 
 export default Post
